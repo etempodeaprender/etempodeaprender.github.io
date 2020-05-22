f9544bbc4f356371ea78d6b60cfc4a4e8267d133
Emanoel Lucas <emanoellucas123456@gmail.com>
2020-05-22 12:57:30 -0300

    .

--- assets/js/comum.js
+++ assets/js/comum.js
@@ -1,5 +1,5 @@
 window.onload = function(){
     let barra = document.querySelector('.barra-atualizacao');
-    let texto = "Última atualização em 04/05/2020, às 02:11."
+    let texto = "Última atualização em 10/05/2020, às 22:03."
     barra.appendChild(document.createTextNode(texto))
 }
