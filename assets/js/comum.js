b117dcc086d6c8ec0121f94eb178c27c3afc3599
É tempo de aprender <emanoellucas123456@gmail.com>
2020-11-03 02:22:33 -0300

    atualizacao 03/011/2020 02:22

--- assets/js/comum.js
+++ assets/js/comum.js
@@ -1,5 +1,5 @@
 window.onload = function(){
     let barra = document.querySelector('.barra-atualizacao');
-    let texto = "Última atualização em 10/05/2020, às 22:03."
+    let texto = "Última atualização em 03/11/2020, às 02:22."
     barra.appendChild(document.createTextNode(texto))
 }
